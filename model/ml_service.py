c7678e0aebf6852bef844b06393b77a5d248e783
ja818 <julianarango1818@gmail.com>
2022-10-25 08:47:03 -0500

    Completed predict function (get model to predict)

--- model/ml_service.py
+++ model/ml_service.py
@@ -9,7 +9,6 @@ from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications.resnet50 import decode_predictions, preprocess_input
 from tensorflow.keras.preprocessing import image
 
-# TODO
 # Connect to Redis and assign to variable `db``
 # Make use of settings.py module to get Redis settings like host, port, etc.
 db = redis.Redis(
@@ -17,11 +16,10 @@ db = redis.Redis(
   port=settings.REDIS_PORT,
   db=settings.REDIS_DB_ID)
 
-# TODO
 # Load your ML model and assign to variable `model`
 # See https://drive.google.com/file/d/1ADuBSE4z2ZVIdn66YDSwxKv-58U7WEOn/view?usp=sharing
 # for more information about how to use this model.
-model = None
+model = ResNet50(include_top=True, weights="imagenet")
 
 
 def predict(image_name):
@@ -40,9 +38,18 @@ def predict(image_name):
         Model predicted class as a string and the corresponding confidence
         score as a number.
     """
-    class_name = "Kelly" + image_name
-    pred_probability = 0.69
+    class_name = None
+    pred_probability = None
     # TODO
+    img = image.load_img(f"./uploads/{image_name}", target_size=(224, 224))
+    x = image.img_to_array(img)
+    x_batch = np.expand_dims(x, axis=0)
+    x_batch = preprocess_input(x_batch)
+    preds = model.predict(x_batch)
+    raw_prediction = decode_predictions(preds, top=1)
+    print(raw_prediction)
+    class_name = raw_prediction[0][0][1]
+    pred_probability = raw_prediction[0][0][2]
 
     return class_name, pred_probability
 
@@ -64,20 +71,21 @@ def classify_process():
         queue_name, job_data = db.brpop(settings.REDIS_QUEUE)
         #   2. Run your ML model on the given data
         job_data = json.loads(job_data)
-        raw_prediction = predict(job_data["image_name"])
+        model_prediction = predict(job_data["image_name"])
         #   3. Store model prediction in a dict with the following shape:
         #      {
         #         "prediction": str,
         #         "score": float,
         #      }
         prediction = {
-          "prediction": raw_prediction[0],
-          "score": raw_prediction[1],
+          "prediction": model_prediction[0],
+          "score": str(model_prediction[1])
         }
         #   4. Store the results on Redis using the original job ID as the key
         #      so the API can match the results it gets to the original job
         #      sent
-        db.set(job_data["id"], json.dumps(prediction))
+        prediction = json.dumps(prediction)
+        db.set(job_data["id"], prediction)
         # Hint: You should be able to successfully implement the communication
         #       code with Redis making use of functions `brpop()` and `set()`.
         # TODO
