2997512295ee70cf62f2fa25f28a4d420176d660
ja818 <julianarango1818@gmail.com>
2022-10-31 16:19:58 -0500

    Compleated stresstest script

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -2,8 +2,14 @@ from locust import HttpUser, task, between
 
 
 class APIUser(HttpUser):
+  wait_time = between(1, 5)
 
-    # Put your stress tests here.
-    # See https://docs.locust.io/en/stable/writing-a-locustfile.html for help.
-    # TODO
-    raise NotImplementedError
+  @task(1)
+  def index(self):
+    self.client.get("http://0.0.0.0")
+  
+  @task(3)
+  def predict(self):
+    files = [("file", ("dog.jpeg", open("dog.jpeg", "rb"), "image/jepg"))]
+    headers = {}
+    self.client.post("http://0.0.0.0/predict", headers=headers, files=files)
